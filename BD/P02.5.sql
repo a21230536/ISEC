816ba4c9272c146129f77825a2d1bfd213c5a986
José Vieira Lisboa <vieira.lisboa@yahoo.com>
2014-10-12 20:24:45 +0100

    Add BD/P02.5.sql

---
+++ BD/P02.5.sql
@@ -0,0 +1,5 @@
+-- 5. Mostre quais os autores que compraram livros na livraria.
+SELECT DISTINCT autores.NOME
+FROM autores, clientes, vendas
+WHERE vendas.CODIGO_CLIENTE = clientes.CODIGO_CLIENTE
+AND autores.N_CONTRIBUINTE = clientes.N_CONTRIBUINTE;
