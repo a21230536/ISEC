81ff2bf84395e8889929ccfe8dcc60feec3e5eee
a21230536 <a21230536@alunos.isec.pt>
2014-10-12 20:31:39 +0100

    Update P02.3.sql

--- BD/P02.3.sql
+++ BD/P02.3.sql
@@ -1,6 +1,6 @@
--- 3. Qual o nome dos autores que escreveram livros do seu género preferido. 
+-- 3. Nomes dos autores que escreveram livros do seu género preferido. 
 SELECT nome
 FROM autores, livros
-WHERE livros.genero = autores.genero_preferido
+WHERE livros.GENERO = autores.GENERO_PREFERIDO
 AND livros.CODIGO_AUTOR = autores.CODIGO_AUTOR
 ORDER BY nome DESC;
