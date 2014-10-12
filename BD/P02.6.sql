0c97793a0c49bfd7d2d6033fc6be05cc68a14460
a21230536 <a21230536@alunos.isec.pt>
2014-10-12 20:33:55 +0100

    Update P02.6.sql

--- BD/P02.6.sql
+++ BD/P02.6.sql
@@ -1,6 +1,6 @@
 -- 6. Mostrar, para cada autor, quais os livros que escreveu cujos géneros não são o género preferido do autor.
 --    Ordenar o resultado pelo nome do autor.
-SELECT nome "NOME DO AUTOR", titulo, genero, autores.GENERO_PREFERIDO
+SELECT nome "NOME DO AUTOR", titulo, genero
 FROM livros, autores
 WHERE livros.codigo_autor = autores.codigo_autor
 AND genero <> genero_preferido -- ou: genero != genero_preferido
