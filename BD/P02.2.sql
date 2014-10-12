dd211ba6b5646fe4426b8c101d73e9c5c59d4616
José Vieira Lisboa <vieira.lisboa@yahoo.com>
2014-10-12 20:24:05 +0100

    Add BD/P02.2.sql

---
+++ BD/P02.2.sql
@@ -0,0 +1,6 @@
+-- 2. Mostre o título dos livros Policiais escritos por autores de Coimbra.
+SELECT livros.TITULO
+FROM autores, livros
+WHERE livros.GENERO = 'Policial'
+AND autores.MORADA LIKE '%Coimbra'
+AND autores.CODIGO_AUTOR = livros.CODIGO_AUTOR;
