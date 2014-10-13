5ced25050e797d0f87f097bfdb5e9f2d8175fba6
a21230536 <a21230536@alunos.isec.pt>
2014-10-13 03:40:06 +0100

    Update P01.4.sql

--- BD/P01/P01.4.sql
+++ BD/P01/P01.4.sql
@@ -2,6 +2,5 @@
 -- 4. Seleccionar o título dos livros cujo preço de tabela esteja entre 25 e 35.
 SELECT titulo
 FROM livros
-WHERE preco_tabela >= 25
-AND preco_tabela <= 35;
---WHERE preco_tabela BETWEEN 25 AND 35;-- equivalente
+WHERE preco_tabela BETWEEN 25 AND 35;
+-- ou: WHERE preco_tabela >= 25 AND preco_tabela <= 35;
