6e8f9c1387b943fb2d5d55ec679cab6a7e579657
José Vieira Lisboa <vieira.lisboa@yahoo.com>
2014-10-14 13:29:09 +0100

    Update Televisao.cpp

--- POO/Ficha 2/Televisao.cpp
+++ POO/Ficha 2/Televisao.cpp
@@ -4,97 +4,97 @@
 using namespace std;
 
 Televisao::Televisao(string canais[], int n){
-	setCanais(canais, n);
-	ligada = false;
-	volume = 0;
-	canal = 1;
+    setCanais(canais, n);
+    ligada = false;
+    volume = 0;
+    canal = 1;
 }
 
 void Televisao::setCanais(string canais0[], int n){
-	if (n > DIM){
-		n = DIM;
-	}
-	for (int i = 0; i < n; i++){
-		canais[i] = canais0[i];
-	}
-	nCanais = n;
+    if (n > DIM){
+        n = DIM;
+    }
+    for (int i = 0; i < n; i++){
+        canais[i] = canais0[i];
+    }
+    nCanais = n;
 }
 
 void Televisao::ligar(){
-	if (!ligada){
-		ligada = true;
-	}
+    if (!ligada){
+        ligada = true;
+    }
 }
 
 void Televisao::desligar(){
-	if (ligada){
-		ligada = false;
-	}
+    if (ligada){
+        ligada = false;
+    }
 }
 
 // Volume
 void Televisao::maisSom(){
-	if (volume < 10){
-		volume++;
-	}
+    if (volume < 10){
+        volume++;
+    }
 }
 void Televisao::menosSom(){
-	if (volume > 0){
-		volume--;
-	}
+    if (volume > 0){
+        volume--;
+    }
 }
 void Televisao::semSom(){
-	if (volume != 0){
-		volume = 0;
-	}
+    if (volume != 0){
+        volume = 0;
+    }
 }
 
 // canais
 void Televisao::canalSeguinte(){
-	if (canal < nCanais){
-		canal++;
-	}
-	else canal = 1;
+    if (canal < nCanais){
+        canal++;
+    }
+    else canal = 1;
 }
 void Televisao::canalAnterior(){
-	if (canal > 1) {
-		canal--;
-	}
-	else canal = nCanais;
+    if (canal > 1) {
+        canal--;
+    }
+    else canal = nCanais;
 }
 void Televisao::setCanal(){
-	int n;
-	string s;
+    int n;
+    string s;
 
-	if (!ligada) return;
+    if (!ligada) return;
 
-	for (int i = 0; i < nCanais; i++){
-		cout << "[" << i + 1 << "] " << canais[i] << endl;
-	}
+    for (int i = 0; i < nCanais; i++){
+        cout << "[" << i + 1 << "] " << canais[i] << endl;
+    }
 
-	cin >> s;
+    cin >> s;
 
-	n = stoi(s);
+    n = stoi(s);
 
-	cout << "n: " << n << endl;
+    cout << "n: " << n << endl;
 
-	if (n > 0 && n <= nCanais){
-		canal = n;
-	}
+    if (n > 0 && n <= nCanais){
+        canal = n;
+    }
 }
 
 // informação
 void Televisao::info(){
-	if (!ligada) {
-		cout << "Televisao Desligada" << endl;
-		return;
-	}
+    if (!ligada) {
+        cout << "Televisao Desligada" << endl;
+        return;
+    }
 
-	cout << "Canal: "
-		<< canal
-		<< " - "
-		<< canais[canal - 1]
-		<< endl
-		<< "Volume: "
-		<< volume << endl;
+    cout << "Canal: "
+        << canal
+        << " - "
+        << canais[canal - 1]
+        << endl
+        << "Volume: "
+        << volume << endl;
 }
