c78f07d71fca970c91b33620b06eec0802cd3461
José Vieira Lisboa <vieira.lisboa@yahoo.com>
2014-10-14 13:29:43 +0100

    Update Televisao.h

--- POO/Ficha 2/Televisao.h
+++ POO/Ficha 2/Televisao.h
@@ -7,35 +7,35 @@
 using namespace std;
 
 class Televisao {
-	static const int DIM = 10;
+    static const int DIM = 10;
 
-	string canais[DIM];
-	int nCanais;
+    string canais[DIM];
+    int nCanais;
 
-	bool ligada;
-	int volume;// min:0 - max:10
-	int canal; // indice actual do array canais
-	void setCanais(string[], int);
+    bool ligada;
+    int volume;// min:0 - max:10
+    int canal; // indice actual do array canais
+    void setCanais(string[], int);
 
 public:
-	// power
-	void ligar();
-	void desligar();
+    // power
+    void ligar();
+    void desligar();
 
-	// volume
-	void maisSom();
-	void menosSom();
-	void semSom();
+    // volume
+    void maisSom();
+    void menosSom();
+    void semSom();
 
-	// canais
-	void canalSeguinte();
-	void canalAnterior();
-	void setCanal();
+    // canais
+    void canalSeguinte();
+    void canalAnterior();
+    void setCanal();
 
-	// informação
-	void info();
+    // informação
+    void info();
 
-	Televisao(string canais[], int n);
+    Televisao(string canais[], int n);
 };
 
 #endif
