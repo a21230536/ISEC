feaf3062e798dbb3bc3591a5dfd7a446a569fb7f
José Vieira Lisboa <vieira.lisboa@yahoo.com>
2014-10-23 04:10:57 +0100

    Update UDP_cliente.c

--- IRC/UDP/UDP_cliente.c
+++ IRC/UDP/UDP_cliente.c
@@ -16,20 +16,47 @@ void Abort(char *msg);
 
 int main( int argc , char *argv[] ){
 	SOCKET sockfd;
-	int msg_len, iResult, nbytes;
+	int iResult, nbytes;
 	struct sockaddr_in serv_addr;
-
-	/* 3. Variáveis necessárias para o exercício 3 */
-	struct sockaddr_in local_name;
-	int namelen;
-
-	char buffer[BUFFERSIZE];
+	char buffer[BUFFERSIZE] = {'\0'};
 	WSADATA wsaData;
 
-	/* TESTAR A SINTAXE */
-	if(argc != 2){
-	    fprintf(stderr,"Sintaxe: %s frase_a_enviar\n",argv[0]);
-	    exit(EXIT_FAILURE);
+	/* variáveis para o exercício 3 */
+	struct sockaddr_in local_name, reply_addr;
+	int sockaddr_in_len;
+
+	/* variáveis para o exercício 6 */
+	int serv_udp_port = 0;
+	char serv_host_addr[16] = {'\0'};
+	char sintaxe[] = "-msg <mensagem> [-ip <server_ip> [-port <server_port>]]";
+	int i;
+
+	/* 6. MENSAGEM/IP/PORTO RECEBIDOS POR ARGUMENTO
+	      a sintaxe anterior (2 argumentos) continua válida */
+	if(argc == 2) strcpy(buffer, argv[1]);
+	else for (i = 1; i < argc; i++) {
+		/* mensagem */
+		if (strcmp("-msg", argv[i]) == 0)
+			strcpy(buffer, argv[++i]);/* ++i para copiar/saltar 1 argumento */
+		/* enderesso ip */
+		else if (strcmp("-ip", argv[i]) == 0)
+			serv_udp_port = atoi(argv[++i]);
+		/* porto */
+		else if (strcmp("-port", argv[i]) == 0)
+			strcpy(serv_host_addr, argv[++i]);
+	}
+	/* usar o porto em SERV_UDP_PORT se não foi recebido por argumento */
+	if (!serv_udp_port) serv_udp_port = SERV_UDP_PORT;
+	/* usar o ip em SERV_HOST_ADDR se não foi recebido por argumento */
+	if (!strlen(serv_host_addr)) strcpy(serv_host_addr, SERV_HOST_ADDR);
+
+	/* TESTAR A SINTAXE
+	   a condição foi alterada para o exercício 6 */
+	if (argc < 2 || !strlen(buffer)){
+		fprintf(stderr, "sintaxe:\n  %s <mensagem>\n", argv[0]);
+		/* Sintaxe 2 foi adicionada para complementar o exercício 6 */
+		fprintf(stderr, "  %s %s\n", argv[0], sintaxe);
+		exit(EXIT_FAILURE);
 	}
 
 	/* INICIAR OS WINSOCKS */
@@ -49,41 +76,59 @@ int main( int argc , char *argv[] ){
 	/* PREENCHER O ENDERECO DO SERVIDOR */
 	memset( (char*)&serv_addr , 0, sizeof(serv_addr) ); /* Coloca a zero todos os bytes */
 	serv_addr.sin_family = AF_INET; /* Address Family: Internet */
-	serv_addr.sin_addr.s_addr = inet_addr(SERV_HOST_ADDR); /* IP no formato "dotted decimal" => 32 bits */
-	serv_addr.sin_port = htons(SERV_UDP_PORT); /* Host TO Netowork Short */
+	serv_addr.sin_addr.s_addr = inet_addr(serv_host_addr); /* IP no formato "dotted decimal" => 32 bits */
+	serv_addr.sin_port = htons(serv_udp_port); /* Host TO Netowork Short */
 
 	/* A sugestão do professor para o exercício 3 é neste lugar...
 	if (bind(sockfd, (SOCKADDR *)&serv_addr, sizeof(serv_addr)) == SOCKET_ERROR) {
 		Abort("Bind Error");
 	} */
 
-	/* ENVIAR MENSAGEM AO SERVIDOR */
-	msg_len = strlen(argv[1]);
-	if(sendto(sockfd , argv[1], msg_len, 0, (struct sockaddr*)&serv_addr , sizeof(serv_addr)) == SOCKET_ERROR){
+	/* ENVIAR MENSAGEM AO SERVIDOR
+	   info da ligação com o servidor guardada na estrutura serv_addr */
+	if (sendto(sockfd, buffer, strlen(buffer), 0,
+			(struct sockaddr*)&serv_addr, sizeof(serv_addr)) == SOCKET_ERROR){
 	    Abort("SO nao conseguiu aceitar o datagrama");
 	}
 
-	/* 3. MOSTRAR O PORTO LOCAL DO SOCKET */
-	namelen = sizeof(local_name);
-	if (getsockname(sockfd, (SOCKADDR *)&local_name, &namelen) == SOCKET_ERROR){
+	/* 3. INFORMAÇÃO LOCAL DO SOCKET */
+	sockaddr_in_len = sizeof(local_name);
+	if (getsockname(sockfd, (SOCKADDR *)&local_name, &sockaddr_in_len) == SOCKET_ERROR){
 	    Abort("Get Socket Name Error");
 	}
-	printf("<CLI1> Porto Local {%d}\n", local_name.sin_port);
+	/* mostrar enderesso local - porto emissor */
+	printf("<CLI1> cliente %s:%d >>>\n", inet_ntoa(local_name.sin_addr), local_name.sin_port);
+	/* mostrar enderesso do servidor - porto receptor */
+	printf("<CLI1> servidor %s:%d <<<\n", serv_host_addr, serv_udp_port);
+	/* mostrar enderesso do servidor - porto emissor */
+	printf("<CLI1> servidor %s:%d >>>\n", inet_ntoa(serv_addr.sin_addr), serv_addr.sin_port);
 
+	printf("<CLI1> mensagem enviada\n<CLI1> a aguadar a confirmacao de entrega\n");
 
-	printf("<CLI1> Mensagem enviada\n<CLI1> A aguadar confirmacao de entrega...\n");
-
-	/* 2. RECEBER A MENSAGEM (DE CONFIRMAÇÃO) DO SERVIDOR E MOSTRÁ-LA */
-	if ((nbytes = recvfrom(sockfd, buffer, sizeof(buffer), 0, NULL, NULL)) == SOCKET_ERROR) {
-	    Abort("Erro na recepcao de datagrams");
+	/* 2. RECEBER A MENSAGEM (DE CONFIRMAÇÃO) DO SERVIDOR E MOSTRÁ-LA
+	      informação da ligação que se recebe guardada na estrutura reply_addr */
+	if ((nbytes = recvfrom(sockfd, buffer, sizeof(buffer), 0,
+			(SOCKADDR *)& reply_addr, &sockaddr_in_len)) == SOCKET_ERROR) {
+	    Abort("erro na recepcao de datagramas");
 	}
 	buffer[nbytes] = '\0'; /* Afixar a string recebida no buffer com '\0' */
-	printf("<SER1> Mensagem \"%s\" recebida.\n", buffer);
+
+	printf("<CLI1> recepcao da mensagem \"%s\" confirmada\n", buffer);
+
+	/* 5. DETECTAR SE A RESPOSTA VEM DE UM IMPOSTOR
+	      comparar a informação entre as estruturas serv_addr (info do servidor)
+		  e reply_addr (info da ligação de qual se recebeu a resposta) */
+	if (inet_ntoa(serv_addr.sin_addr) == inet_ntoa(reply_addr.sin_addr)
+			&& serv_addr.sin_port == reply_addr.sin_port)
+		printf("<CLI1> servidor %s:%d >>> OK\n",
+			inet_ntoa(reply_addr.sin_addr), reply_addr.sin_port);
+	else
+		printf("<CLI1> emissor %s:%d >>> IMPOSTOR\n",
+			inet_ntoa(reply_addr.sin_addr), reply_addr.sin_port);
 
 	/* FECHAR O SOCKET */
 	closesocket(sockfd);
 
-	printf("\n");
 	exit(EXIT_SUCCESS);
 }
 
@@ -91,6 +136,6 @@ int main( int argc , char *argv[] ){
    Mostra uma mensagem de erro e o código associado ao ultimo erro com Winsocks. 
    Termina a aplicacao com "exit status" a 1 (constante EXIT_FAILURE) */
 void Abort(char *msg) {
-	fprintf(stderr,"<CLI1>Erro fatal: <%s> (%d)\n",msg, WSAGetLastError());
+	fprintf(stderr,"<CLI1>Erro Fatal: <%s> (%d)\n",msg, WSAGetLastError());
 	exit(EXIT_FAILURE);
 }
