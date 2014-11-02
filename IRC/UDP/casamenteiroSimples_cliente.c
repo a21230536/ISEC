0d991afdb5e8f9e028b9fa36699e0627cd479bf0
José Vieira Lisboa <vieira.lisboa@yahoo.com>
2014-11-02 16:06:02 +0000

    Update IRC UDP casamenteiro simples

--- IRC/UDP/casamenteiroSimples_cliente.c
+++ IRC/UDP/casamenteiroSimples_cliente.c
@@ -15,8 +15,9 @@ main(int argc, char *argv[])
     int wsa_result, sa_len = sizeof(struct sockaddr_in);
     char msg[256];
 
+    /* verificar os argumentos */
     if (argc < 4) {
-        printf("> Uso: %s <mensagem> <endereco ip> <porto>\n", argv[0]);
+        printf("> Sintaxe: %s <mensagem> <endereco ip> <porto>\n", argv[0]);
         exit(1);
     }
 
@@ -37,12 +38,12 @@ main(int argc, char *argv[])
     servidor.sin_addr.s_addr = inet_addr(argv[2]);
     servidor.sin_port = htons(atoi(argv[3]));
 
-    /* enviar um datagrama ao Servidor */
+    /* enviar a mensagem ao Servidor */
     if (sendto(sock, argv[1], strlen(argv[1]), 0, (SOCKADDR*)&servidor, sa_len) == SOCKET_ERROR) {
         die("Falha a Enviar Mensagem ao Servidor");
     }
 
-    /* receber um datagrama (estrutura sockaddr_in) */
+    /* receber datagrama (estrutura sockaddr_in) */
     puts("> esperando o meu par");
     if (recvfrom(sock, &par, sa_len, 0, (SOCKADDR*)&remetente, &sa_len) == SOCKET_ERROR) {
         die("Falha a Receber Mensagem");
@@ -50,10 +51,10 @@ main(int argc, char *argv[])
 
     /* verificar de quem se recebeu o datagrama */
     if (strcmp(argv[2], inet_ntoa(remetente.sin_addr)) == 0 && atoi(argv[3]) == ntohs(remetente.sin_port)) {
-        /* o datagrama (endereço do cliente A) veio do servidor */
+        /* o datagrama (com o endereço do cliente A) veio do servidor */
         printf("> o meu par (cliente 1) - %s:%d\n", inet_ntoa(par.sin_addr), ntohs(par.sin_port));
         puts("> a contactar o meu par ...");
-        /* enviar (encaminhar) o datagrama ao cliente A */
+        /* enviar o datagrama ao cliente A */
         if (sendto(sock, &par, sa_len, 0, (SOCKADDR*)&par, sa_len) == SOCKET_ERROR) {
             die("Falha a Enviar Mensagem ao Par");
         }
