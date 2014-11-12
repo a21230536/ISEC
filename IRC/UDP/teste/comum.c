59e5e6c7c9bd518cac7a809832c59e4daf6b9eff
José Vieira Lisboa <vieira.lisboa@yahoo.com>
2014-11-12 03:06:08 +0000

    Add IRC UDP mini-teste

---
+++ IRC/UDP/teste/comum.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <winsock.h>
+#include <string.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+void die(char *msg)
+{
+    fprintf(stderr, "<CLI1> ERRO <%s> (%d)\n", msg, WSAGetLastError());
+    exit(EXIT_FAILURE);
+}
+
+void sai(char *msg)
+{
+    fprintf(stderr, "ERRO [%s]\n", msg);
+    exit(1);
+}
+
+char *erro(char *msg)
+{
+    char errname[32];
+    int errcode = WSAGetLastError();
+
+    switch (errcode) {
+        case 10060:
+            strcpy(errname, "TIMEOUT");
+            break;
+        default:
+            strcpy(errname, "?");
+    }
+
+    sprintf(msg, "WSA ERROR %d (%s)", errcode, errname);
+    return msg;
+}
+
+void falha(char *s)
+{
+    char wsaerr[32], msg[256];
+
+    erro(wsaerr);
+    sprintf(msg, "FALHA <%s> %s", s, wsaerr);
+    sai(msg);
+}
+
+void supper(char *s)
+{
+    int i, n = strlen(s);
+
+    for(i=0; i<n; i++) {
+        s[i] = toupper(s[i]);
+    }
+}
---
+++ IRC/UDP/teste/comum.h
@@ -0,0 +1,24 @@
+#ifndef COMUM_H
+
+#define COMUM_H
+#define SERVER_PORT 5432
+#define BUFFER_SIZE 256
+#define SERVER_ADDR "127.0.0.1"
+#define TIMEOUT 10
+#define N 5
+#define TRUE 1
+#define FALSE 0
+
+typedef struct _res {
+    char msg[256];
+    int casamento;
+    struct sockaddr_in noivo;
+} RES;
+
+void die(char *);
+void sai(char *);
+char *erro(char *);
+void falha(char *);
+void supper(char *);
+
+#endif
