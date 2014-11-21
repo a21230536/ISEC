6ab0d62fe580963c558da567e156ba55b595192d
José Vieira Lisboa <vieira.lisboa@yahoo.com>
2014-11-21 23:37:45 +0000

    Add P08.4

---
+++ BD/P08/P08.4.sql
@@ -0,0 +1,16 @@
+-- FICHA 8
+-- 4
+-- Como ao criar a nova tabela LIVROS_BACKUP as restrições não são copiadas altere a
+-- tabela de modo a incluir as restrições que existiam na tabela LIVROS original. Use o
+-- comando ALTER TABLE.
+
+ALTER TABLE livros_backup
+ADD CONSTRAINT codigo_livro_pk PRIMARY KEY (codigo_livro);
+
+ALTER TABLE livros_backup
+ADD CONSTRAINT codigoeditora_fk FOREIGN KEY (codigo_editora)
+    REFERENCES editoras (codigo_editora);
+
+ALTER TABLE livros_backup
+ADD CONSTRAINT codigo_autor_fk FOREIGN KEY (codigo_autor)
+    REFERENCES autores (codigo_autor);
