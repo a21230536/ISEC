95bc83de509f614aa80deb0f27aa22b153bc62c2
José Vieira Lisboa <vieira.lisboa@yahoo.com>
2014-11-22 00:52:44 +0000

    Add BD P08.6

---
+++ BD/P08/P08.6.sql
@@ -0,0 +1,7 @@
+-- FICHA 8
+-- 6
+-- Insira na tabela AUTORES2 um novo autor ‘José de Magalhães’ com o código 45 e
+-- contribuinte 77665544.
+
+INSERT INTO autores2 (codigo_autor, nome, n_contribuinte)
+VALUES (45, UPPER('José de Magalhães'), 77665544);
