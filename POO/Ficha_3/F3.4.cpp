488c0d0d8bddf9b31c52a73c2748072732820c82
José Vieira Lisboa <vieira.lisboa@yahoo.com>
2014-11-25 15:35:25 +0000

    Update POO F3.4.cpp

--- POO/Ficha_3/F3.4.cpp
+++ POO/Ficha_3/F3.4.cpp
@@ -5,44 +5,44 @@ using namespace std;
 
 int main()
 {
-	//Ponto::log = true;
-	//Rectangulo::log = true;
-	Desenho::log = true;
+    //Ponto::log = true;
+    //Rectangulo::log = true;
+    Desenho::log = true;
 
-	// criar um desemho
-	Desenho desenho;
+    // criar um desemho
+    Desenho desenho;
     cout << "> desenho.areas();// " << desenho.areas() << endl;
 
-	// testar (i) adicionar retângulos ao desenho
-	desenho.retangulo(Rectangulo(Ponto(0, 0), 7, 8));// 1
-	cout << "> desenho.areas();// " << desenho.areas() << endl;
-	Rectangulo retangulo(Ponto(3, 3), 13, 3);// 2
-	if (!desenho.retangulo(retangulo)) {// retângulo intersecta outro existente
+    // testar (i) adicionar retângulos ao desenho
+    desenho.retangulo(Rectangulo(Ponto(0, 0), 7, 8));// 1
+    cout << "> desenho.areas();// " << desenho.areas() << endl;
+    Rectangulo retangulo(Ponto(3, 3), 13, 3);// 2
+    if (!desenho.retangulo(retangulo)) {// retângulo intersecta outro existente
         cout << "> INTERSECTA! Retangulo " << retangulo.str() << " NAO adicionado ao desenho." << endl;
     }
 
-	// adicionar dois retângulos no mesmo ponto...
-	Ponto P(10, 17);
+    // adicionar dois retângulos no mesmo ponto...
+    Ponto P(10, 17);
     desenho.retangulo(Rectangulo(P, 2, 8));// 3
-	desenho.retangulo(Rectangulo(P, 4, 9));// 4
-	
-	// testar (ii) obter um conjunto de retângulos no mesmo ponto
-	vector<Rectangulo> conjunto = desenho.conjunto(P);
-	for (Rectangulo elemento : conjunto) {
-		cout << "> elemento.str();" << elemento.str() << endl;
-	}
+    desenho.retangulo(Rectangulo(P, 4, 9));// 4
+    
+    // testar (ii) obter um conjunto de retângulos no mesmo ponto
+    vector<Rectangulo> conjunto = desenho.conjunto(P);
+    for (Rectangulo elemento : conjunto) {
+        cout << "> elemento.str();" << elemento.str() << endl;
+    }
 
-	// testar (iii) eliminar retângulos com area superior a 50
-	cout << "> desenho.eliminar(50);" << endl;
-	int n = desenho.eliminar_area_superior(50);
-	cout << "> fo" << (n == 1? "i eliminado " : "ram eliminados ") << n << " retangulo" << (n != 1? "s" : "") 
-		 << endl;
+    // testar (iii) eliminar retângulos com area superior a 50
+    cout << "> desenho.eliminar(50);" << endl;
+    int n = desenho.eliminar_area_superior(50);
+    cout << "> fo" << (n == 1? "i eliminado " : "ram eliminados ") << n << " retangulo" << (n != 1? "s" : "") 
+         << endl;
 
-	// testar (iv) obter a área total dos retângulos
-	cout << "> desenho.areas();// " << desenho.areas() << endl;
+    // testar (iv) obter a área total dos retângulos
+    cout << "> desenho.areas();// " << desenho.areas() << endl;
 
-	// testar (v) obter informação sobre o desenho
-	cout << "> desenho.str();// " << desenho.str() << endl;
+    // testar (v) obter informação sobre o desenho
+    cout << "> desenho.str();// " << desenho.str() << endl;
 
-	return 0;
+    return 0;
 }
