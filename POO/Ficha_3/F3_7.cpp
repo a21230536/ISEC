92ff90d7335eb142f08d38f41907894768d6ad7c
José Vieira Lisboa <vieira.lisboa@yahoo.com>
2014-11-26 16:24:59 +0000

    Update POO F3.7 c

--- POO/Ficha_3/F3_7.cpp
+++ POO/Ficha_3/F3_7.cpp
@@ -1,39 +1,47 @@
 #include <iostream>
-#include "conta.h"
+#include "banco.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "> Pessoa ze(\"Jose Daniel\", \"0999999-9XY\", \"999 999 999\");" << endl;
+    cout << "> Pessoa joe(\"Jose Daniel\", \"0999999-9XY\", \"999 999 999\");" << endl;
     Pessoa joe("Jose Daniel", "0999999-9XY", "999 999 999");
 
     // b) classe Conta
     cout << "> cgd(&joe);" << endl;
     Conta conta(&joe);
     cout << boolalpha;
-    // saldo inicial
-    cout << "> cgd.saldo();// " << conta.saldo() << endl;
-    // tentar adicionar -1 (tirar 1) ao saldo
-    cout << "> cgd.credito(-1);// " << conta.credito(-1) << endl;
-    // adicionar 3 ao saldo
-    cout << "> cgd.credito(3);// " << conta.credito(3) << endl;
-    // verificar saldo
+    cout << "> cgd.saldo();// " << conta.saldo() << endl;// 0
+    cout << "> cgd.credito(-1);// " << conta.credito(-1) << endl;// false
+    cout << "> cgd.credito(3);// " << conta.credito(3) << endl;// true
     cout << "> cgd.saldo();// " << conta.saldo() << endl;// 3
-    // tentar remover 5 do saldo
     cout << "> cgd.debito(5);// " << conta.debito(5) << endl;// false
-    // remover 1 do saldo
     cout << "> cgd.debito(1);// " << conta.debito(1) << endl;// true
-    // verificar o saldo
     cout << "> cgd.saldo();// " << conta.saldo() << endl;// 2
-    // obter o titular da conta
     Pessoa const *titular = conta.titular();
-    // tentar modificar o titular
     //titular->nome("Zorro");// ERRO! titular aponta a uma constante
-    // mostrar info do titular
     cout << "> TITULAR: " << titular->nome()
          << "; BI: " << titular->bi()
          << "; NIF: " << titular->nif() << endl;
 
+	// c) classe Banco
+	Banco caixa("Caixa");
+	cout << "> caixa.nomes();// " << caixa.nomes() << endl;
+	cout << "> caixa.soma();// " << caixa.soma() << endl;
+	cout << "> caixa.conta(&joe);// " << caixa.conta(&joe) << endl;
+	cout << "> caixa.conta(joe.bi(), 804);// " << caixa.movimento(joe.bi(), 804) << endl;
+	cout << "> caixa.nomes();// " << caixa.nomes() << endl;
+	cout << "> caixa.soma();// " << caixa.soma() << endl;
+	cout << "> Pessoa maria(\"Maria Silva\", \"0000000-0YY\", \"000 000 000\");" << endl;
+	Pessoa maria("Maria Silva", "0000000-0YY", "000 000 000");
+	cout << "> caixa.conta(&maria);// " << caixa.conta(&maria) << endl;
+	cout << "> caixa.conta(maria.bi(), 1200);// " << caixa.movimento(maria.bi(), 1200) << endl;
+	cout << "> caixa.conta(maria.bi(), -200);// " << caixa.movimento(maria.bi(), -200) << endl;
+	cout << "> caixa.saldo(maria.bi());// " << caixa.saldo(maria.bi()) << endl;
+	cout << "> caixa.conta(joe.bi())->saldo();// " << caixa.conta(joe.bi()).saldo() << endl;
+	cout << "> caixa.soma();// " << caixa.soma() << endl;
+	cout << "> caixa.nomes();// " << caixa.nomes() << endl;
+	
     return 0;
 }
