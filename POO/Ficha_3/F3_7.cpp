1cf75dc8e59fbc9a0bd1272a6ae4f5b550a9d0a6
José Vieira Lisboa <vieira.lisboa@yahoo.com>
2014-11-26 14:06:54 +0000

    Update POO F3.7

--- POO/Ficha_3/F3_7.cpp
+++ POO/Ficha_3/F3_7.cpp
@@ -10,24 +10,24 @@ int main()
 
     // b) classe Conta
     cout << "> cgd(&joe);" << endl;
-    Conta caixa(&joe);
+    Conta conta(&joe);
     cout << boolalpha;
     // saldo inicial
-    cout << "> cgd.saldo();// " << caixa.saldo() << endl;
+    cout << "> cgd.saldo();// " << conta.saldo() << endl;
     // tentar adicionar -1 (tirar 1) ao saldo
-    cout << "> cgd.credito(-1);// " << caixa.credito(-1) << endl;
+    cout << "> cgd.credito(-1);// " << conta.credito(-1) << endl;
     // adicionar 3 ao saldo
-    cout << "> cgd.credito(3);// " << caixa.credito(3) << endl;
+    cout << "> cgd.credito(3);// " << conta.credito(3) << endl;
     // verificar saldo
-    cout << "> cgd.saldo();// " << caixa.saldo() << endl;// 3
+    cout << "> cgd.saldo();// " << conta.saldo() << endl;// 3
     // tentar remover 5 do saldo
-    cout << "> cgd.debito(5);// " << caixa.debito(5) << endl;// false
+    cout << "> cgd.debito(5);// " << conta.debito(5) << endl;// false
     // remover 1 do saldo
-    cout << "> cgd.debito(1);// " << caixa.debito(1) << endl;// true
+    cout << "> cgd.debito(1);// " << conta.debito(1) << endl;// true
     // verificar o saldo
-    cout << "> cgd.saldo();// " << caixa.saldo() << endl;// 2
+    cout << "> cgd.saldo();// " << conta.saldo() << endl;// 2
     // obter o titular da conta
-    Pessoa const *titular = caixa.titular();
+    Pessoa const *titular = conta.titular();
     // tentar modificar o titular
     //titular->nome("Zorro");// ERRO! titular aponta a uma constante
     // mostrar info do titular
