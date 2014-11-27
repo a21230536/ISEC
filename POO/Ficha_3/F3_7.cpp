e29fb60ab6411b6920dbfb513725a051c1f9e434
José Vieira Lisboa <vieira.lisboa@yahoo.com>
2014-11-27 20:55:18 +0000

    Update POO F3.7 e

--- POO/Ficha_3/F3_7.cpp
+++ POO/Ficha_3/F3_7.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <fstream>
 #include "banco.h"
+
+const string FICHEIRO = "c:\\htdocs\\ISEC\\POO\\Ficha_3\\pessoas.txt";
+const int N = 10;
 
 using namespace std;
 
@@ -30,18 +34,87 @@ int main()
     cout << "> caixa.nomes();// " << caixa.nomes() << endl;
     cout << "> caixa.soma();// " << caixa.soma() << endl;
     cout << "> caixa.conta(&joe);// " << caixa.conta(&joe) << endl;
-    cout << "> caixa.conta(joe.bi(), 804);// " << caixa.movimento(joe.bi(), 804) << endl;
+    cout << "> caixa.movimento(joe.bi(), 804);// " << caixa.movimento(joe.bi(), 804) << endl;
     cout << "> caixa.nomes();// " << caixa.nomes() << endl;
     cout << "> caixa.soma();// " << caixa.soma() << endl;
     cout << "> Pessoa maria(\"Maria Silva\", \"0000000-0YY\", \"000 000 000\");" << endl;
     Pessoa maria("Maria Silva", "0000000-0YY", "000 000 000");
     cout << "> caixa.conta(&maria);// " << caixa.conta(&maria) << endl;
-    cout << "> caixa.conta(maria.bi(), 1200);// " << caixa.movimento(maria.bi(), 1200) << endl;
-    cout << "> caixa.conta(maria.bi(), -200);// " << caixa.movimento(maria.bi(), -200) << endl;
+    cout << "> caixa.movimento(maria.bi(), 1200);// " << caixa.movimento(maria.bi(), 1200) << endl;
+    cout << "> caixa.movimento(maria.bi(), -200);// " << caixa.movimento(maria.bi(), -200) << endl;
     cout << "> caixa.saldo(maria.bi());// " << caixa.saldo(maria.bi()) << endl;
     cout << "> caixa.conta(joe.bi())->saldo();// " << caixa.conta(joe.bi()).saldo() << endl;
     cout << "> caixa.soma();// " << caixa.soma() << endl;
     cout << "> caixa.nomes();// " << caixa.nomes() << endl;
+
+    // d)
+    Banco branca("Dona Branca");// banco 2
+    Pessoa manel("Manuel Maria", "0000001-1XX", "111 111 111");
+    Pessoa toze("Antonio Jose Oliveira", "0000010-1XX", "222 222 222");
+    Pessoa rita("Maria Rita", "0000011-0XY", "333 333 333");
+    branca.conta(&manel);
+    branca.conta(&toze);
+    branca.conta(&rita);
+    branca.conta(&maria);
+    caixa.conta(&rita);
+    cout << "> branca.nomes();// " << branca.nomes() << endl;
+    cout << "> caixa.nomes();// " << caixa.nomes() << endl;
+    maria.nome("Maria Augusta da Silva");
+    manel.nome("Manuel Maria Antunes");
+    cout << "> branca.nomes();// " << branca.nomes() << endl;
+    cout << "> caixa.nomes();// " << caixa.nomes() << endl;
+
+    // e)
+    fstream dados;
+    dados.open(FICHEIRO, ios::in);
+    if (!dados) {
+        cout << "> ERRO a abrir o ficheiro " << FICHEIRO << endl;
+        return 1;
+    }
+    cout << "> OBTER PESSOAS DO FICHEIRO " << FICHEIRO << endl;
+    int n = 0;
+    string nome, bi, nif;
+    vector<Pessoa> pessoas;
+    while (n < N && dados >> nome) {
+        dados >> bi;
+        dados >> nif;
+        pessoas.push_back(Pessoa(nome, bi, nif));
+        n++;
+    }
+    dados.close();
+    for (int i = 0; i < n; i++) {
+        caixa.conta(&pessoas[i]);
+        caixa.movimento(pessoas[i].bi(), 1000);
+        branca.conta(&pessoas[i]);
+        branca.movimento(pessoas[i].bi(), 1500);
+        cout << "> " << pessoas[i].nome() << endl;
+    }
+    cout << "> LISTAR OS NOMES DOS CLIENTES DOS BANCOS" << endl;
+    cout << "> branca.nomes();// " << branca.nomes() << endl;
+    cout << "> caixa.nomes();// " << caixa.nomes() << endl;
+    cout << "> ALTERAR NOMES DE PESSOAS" << endl;
+    for (int i = 0; i < n; i++) {
+        nome = pessoas[i].nome() + " Carvalho";
+        switch (i) {
+        case 0:
+            cout << "> pessoas[0].nome(\"" << nome << "\");" << endl;
+            pessoas[0].nome(nome);
+            break;
+        case 1:
+            cout << "> pessoas[1].nome(\"" << nome << "\");" << endl;
+            pessoas[1].nome(nome);
+            break;
+        case 2:
+            cout << "> pessoas[2].nome(\"" << nome << "\");" << endl;
+            pessoas[2].nome(nome);
+        default:;
+        }
+    }
+    cout << "> LISTAR OS NOMES DOS CLIENTES DOS BANCOS (APOS ALTERACAO DE NOMES)" << endl;
+    cout << "> branca.nomes();// " << branca.nomes() << endl;
+    cout << "> caixa.nomes();// " << caixa.nomes() << endl;
+    cout << "> caixa.soma();// " << caixa.soma() << endl;
+    cout << "> branca.soma();// " << branca.soma() << endl;
 
     return 0;
 }
